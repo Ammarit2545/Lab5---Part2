0bc9af8706d47350046dddf519e87f3bab289d2d
Arm <armm4sonic@gmail.com>
2022-07-28 21:38:57 +0700

    21th commit - Make DBMS is Real Value

--- lib/database/transaction_db.dart
+++ lib/database/transaction_db.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/foundation.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 
 class TransactionDB {
   String dbName;
@@ -11,6 +13,11 @@ class TransactionDB {
   Future<String> openDatabase() async {
     Directory appDirectory = await getApplicationDocumentsDirectory();
     String dbLacation = join(appDirectory.path, dbName);
-    return dbLacation;
+    //Make DBMS
+
+    DatabaseFactory dbFactory = await databaseFactoryIo;
+    Future<Database> db = await dbFactory.openDatabase(dbLacation);
+
+    return db;
   }
 }
